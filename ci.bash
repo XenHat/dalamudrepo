99fb15793baf3c460f06abdcf8120bd988e4eb5d
XenHat <me@xenh.at>
2023-07-19 21:55:18 -0400

    remove ascclemens as it's already an aggregate

--- ci.bash
+++ ci.bash
@@ -1,5 +1,4 @@
 {
-	wget -qO- 'https://git.anna.lgbt/ascclemens/plugin_repo/raw/branch/main/unofficial.json'
 	wget -qO- 'https://raw.githubusercontent.com/SF-XIV/dalamud-plugins/main/repo.json'
 	wget -qO- 'https://raw.githubusercontent.com/LeonBlade/DalamudPlugins/main/repo.json'
 	wget -qO- 'https://raw.githubusercontent.com/Aida-Enna/XIVPlugins/main/repo.json'
