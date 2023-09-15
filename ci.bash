f0a8afef3175a1ba22f5c16b185941aa4e2285cf
Xenhat <me@xenh.at>
2023-09-15 18:06:05 +0000

    add more steps to debug the script

--- ci.bash
+++ ci.bash
@@ -3,5 +3,7 @@
 	wget -qO- 'https://raw.githubusercontent.com/LeonBlade/DalamudPlugins/main/repo.json'
 	wget -qO- 'https://raw.githubusercontent.com/Aida-Enna/XIVPlugins/main/repo.json'
 } >tmp.json
-jq <tmp.json '.[]' | jq -s | jq 'del(.[] | select(.Name == "Fullscreen Toggle"))' >repo.json
+jq <tmp.json '.[]' | jq -s | jq 'del(.[] | select(.Name == "Fullscreen Toggle"))' > new_repo.json
+diff repo.json new_repo.json
+mv new_repo.json repo.json
 rm tmp*.json
