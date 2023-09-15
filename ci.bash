0467e2c9262ce5f1d6c723470ae968d41635a684
Xenhat <me@xenh.at>
2023-09-15 19:53:03 +0000

    Update ci.bash

--- ci.bash
+++ ci.bash
@@ -7,7 +7,6 @@ echo "Downloading repos"
 	wget -qO- 'https://raw.githubusercontent.com/SF-XIV/dalamud-plugins/main/repo.json'
 	wget -qO- 'https://raw.githubusercontent.com/LeonBlade/DalamudPlugins/main/repo.json'
 	wget -qO- 'https://raw.githubusercontent.com/Aida-Enna/XIVPlugins/main/repo.json'
-	wget -qO- 'https://git.anna.lgbt/ascclemens/plugin_repo/raw/branch/main/unofficial.json'
 	wget -qO- 'https://raw.githubusercontent.com/NightmareXIV/MyDalamudPlugins/main/pluginmaster.json'
 	wget -qO- 'https://raw.githubusercontent.com/arcticnw/DalamudPlugins/main/repo.json'
 
