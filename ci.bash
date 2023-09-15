bb24eafb7812de2c45b21dd802abb4420089bcb7
Xenhat <me@xenh.at>
2023-09-15 19:16:48 +0000

    bleh

--- ci.bash
+++ ci.bash
@@ -1,10 +1,15 @@
-command -v jq || exit 1
+echo "Checking for JQ"
+command -v jq
+echo "Downloading repos"
 {
 	wget -qO- 'https://raw.githubusercontent.com/SF-XIV/dalamud-plugins/main/repo.json'
 	wget -qO- 'https://raw.githubusercontent.com/LeonBlade/DalamudPlugins/main/repo.json'
 	wget -qO- 'https://raw.githubusercontent.com/Aida-Enna/XIVPlugins/main/repo.json'
 } >tmp.json
+echo "re-formatting new JSON file"
 jq <tmp.json '.[]' | jq -s | jq 'del(.[] | select(.Name == "Fullscreen Toggle"))' > new_repo.json
-diff repo.json new_repo.json || echo "No changes..."
-mv new_repo.json repo.json
-rm tmp*.json
+echo "Checking for differences"
+diff repo.json new_repo.json
+echo "overwriting repo JSON with new file"
+mv -v new_repo.json repo.json
+rm -v tmp*.json
