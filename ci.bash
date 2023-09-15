24350fd69c40dccd33549e59fc75ecdf2cbae600
Xenhat <me@xenh.at>
2023-09-15 19:31:55 +0000

    re-add ascclemens

--- ci.bash
+++ ci.bash
@@ -7,8 +7,11 @@ echo "Downloading repos"
 	wget -qO- 'https://raw.githubusercontent.com/SF-XIV/dalamud-plugins/main/repo.json'
 	wget -qO- 'https://raw.githubusercontent.com/LeonBlade/DalamudPlugins/main/repo.json'
 	wget -qO- 'https://raw.githubusercontent.com/Aida-Enna/XIVPlugins/main/repo.json'
+	wget -qO- 'https://git.anna.lgbt/ascclemens/plugin_repo/raw/branch/main/unofficial.json'
+
 } >tmp.json
 echo "re-formatting new JSON file"
+# FIXME: Make sure plugins don't get added twice from multiple repos by comparing plugin name value
 jq <tmp.json '.[]' | jq -s | jq 'del(.[] | select(.Name == "Fullscreen Toggle"))' > new_repo.json
 echo "Checking for differences"
 diff repo.json new_repo.json
