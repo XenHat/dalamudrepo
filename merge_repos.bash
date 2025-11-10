8cddd47075c974761dedeb2a77f42f567660889f
XenHat <me@xenh.at>
2025-11-10 11:15:25 -0500

    dos2unix...

--- merge_repos.bash
+++ merge_repos.bash
@@ -3,7 +3,9 @@ index=0
 
 while IFS= read -r line; do
 	wget -qO- "$line" >"repo.${index}.tmp.json"
+	wget -O "repo.${index}.tmp.json" "$line"
 	((index++))
 done <repolist
 jq -s 'add' ./*.tmp.json >new_repo.json
-rm ./*.tmp.json
+jq -s 'add' ./repo.*.tmp.json >new_repo.json
+rm ./repo.*.tmp.json
