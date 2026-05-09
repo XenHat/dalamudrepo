e24b031db24b95b03ee1bb1fea1c8758462c13c0
XenHat <me@xenh.at>
2026-05-09 00:12:27 -0400

    organize repo files

--- replace_repos.bash
+++ replace_repos.bash
@@ -1,17 +1,39 @@
 #!/bin/env bash
 INSTALLED_PLUGINS_LOCATION="$HOME/.xlcore/installedPlugins"
-pushd "$INSTALLED_PLUGINS_LOCATION" || exit 1
 # Define the new URL you want to set
 NEW_URL="https://raw.githubusercontent.com/XenHat/dalamudrepo/refs/heads/main/repo.json"
-
-for f in $(fd --type f --glob "*.json" | grep -v ".deps.json"); do
-	if [[ -r "$f" ]]; then
-		if ! grep -q '"InstalledFromUrl": *"\(OFFICIAL\|official\)"' "$f" && ! grep -q '"InstalledFromUrl": "https://raw.githubusercontent.com/Ottermandias/SeaOfStars/main/repo.json"' "$f"; then
-			# Replace the URL using sed
-			sed -i "s|\"InstalledFromUrl\": \".*\"|\"InstalledFromUrl\": \"$NEW_URL\"|" "$f"
+IFS= read -r -d '' repolist <./repolist
+pushd "$INSTALLED_PLUGINS_LOCATION" || exit 1
+for d in */; do
+	foldername=${d%%"/"*}
+	for j in $(find . -name "${foldername}.json"); do
+		file="$j"
+		if [[ -r "$file" ]]; then
+			value=$(jq -r '.InstalledFromUrl' "$file")
+			if [[ "$value" == "null" ]]; then
+				continue
+			fi
+			if [[ "$value" == "$NEW_URL" ]]; then
+				continue
+			fi
+			if [[ "$value" == "OFFICIAL" ]]; then
+				continue
+			fi
+			if [[ "$value" == "https://raw.githubusercontent.com/Ottermandias/SeaOfStars/main/repo.json" ]]; then
+				continue
+			fi
+			# Use echo to pass the multiline string to while loop
+			echo -e "$repolist" | while IFS=$'\n' read -r line; do
+				# Check if the current line contains the search variable
+				if [[ $value == *$line* ]]; then
+					# Replace the URL using sed
+					echo "Replacing repository for plugin '$foldername' ($file)"
+					sed -i "s|\"InstalledFromUrl\": \".*\"|\"InstalledFromUrl\": \"$NEW_URL\"|" "$file"
+				fi
+			done
+		else
+			echo "$file is not readable"
 		fi
-	else
-		echo "$f is not readable"
-	fi
+	done
 done
 popd || return
